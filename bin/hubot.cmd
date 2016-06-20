3191a14687828a592d38ab2580713ed9048876ed
Anthony Shaw <anthonyshaw@apache.org>
2016-06-20 12:02:45 +1000

    sort out readme

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,4 +4,4 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "my-bot" %* 
+node_modules\.bin\hubot.cmd --name "servicenow-bot" %* 
